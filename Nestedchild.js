28973c4eb518b8e897c854421700f1438401c3f4
mitridates <mitridates@gmail.com>
2019-12-12 17:12:07 +0100

    Create Nestedchild.js

---
+++ Nestedchild.js
@@ -0,0 +1,94 @@
+/**
+ * @fileOverview Constructor Nestedchild
+ * @author <a href="mailto:mitridates@gmail.com">Mitridates</a>
+ * @version 1.0
+ */
+
+/**
+ *
+ * @class Clase para crear nested select
+ * @constructor
+ * @name Nestedchild
+ * @param {*} selector
+ */
+ function Nestedchild (selector) {
+    if (!(this instanceof Nestedchild)) return new Nestedchild(selector)
+    this._selector  = this.setSelector(selector);
+}
+/**
+ * Establece el selector actual
+ * @name Nestedchild#setSelector
+ * @method
+ * @param {*} selector
+ * @returns this
+ */
+Nestedchild.prototype.setSelector = function(selector){
+    if(typeof selector == "object" ) return selector;
+    if(typeof selector == "string" ){
+      if(selector.startWith('.')) return  document.querySelectorAll(selector);
+      if(selector.startWith('#')) return  document.getElementById(selector);
+    }
+    console.log('Unknow selector "'+ selector +'" en Nestedchild');
+};
+
+
+
+
+
+/**
+ * Immediately-Invoked Function Expression (IIFE).
+ * @function
+ * @param {Object} Grot - Global window object.
+ * @returns {Object} window.Grot.cache
+ */
+(function() {
+    /**
+     * No exponemos variables
+     * @var {Object} _data
+     */   
+    var _data = {};
+    /**
+     * Cache global para la página actual. IIEF para no exponer variables de forma global.
+     * @name Grot.cache
+     * @function
+     */
+    Grot.cache = {
+        /**
+         * Get cache key
+         * @name Grot.cache#get
+         * @param {string} key
+         */
+        get: function(key) {
+        return _data.hasOwnProperty(key)? _data[key] : null ;
+        },
+        /**
+         * @param {string} key
+         * @param {*} value
+         */
+        add: function(key, value) {
+            var data =  _data.hasOwnProperty(key)? _data[key] : [] ;
+            if(data instanceof Array){
+                data.push(value);
+                return this.set(key, data);
+            }else{
+                console.log('Tipo de dato equivocado '+(typeof data)+', se esperaba "Array"');
+            }
+        },
+        /**
+         * @param {string} key
+         * @param {*} value
+         */
+        set: function(key, value) {
+         _data[key] = value;
+         return this;//chaining
+        },
+        /**
+         * @param {string} key
+         */
+        unset: function(key){
+            if(_data.indexOf(key)!==-1) delete _data[key];
+            else   console.log('Index "'+ key +'" no definido en cache');
+            return this;
+        }  
+    };
+})(window.Grot = window.Grot || {});
