f56474e33d06ba5312d035811b96a64ea2b229b5
mitridates <mitridates@gmail.com>
2019-12-25 21:07:41 +0100

    Update Nestedchild.js

--- Nestedchild.js
+++ Nestedchild.js
@@ -19,21 +19,56 @@
   }
 
 /**
+ * @name Repoblar#getChild
+ * @method
+ * @param {Object} el element type SELECT 
+ * @returns {Object|null} 
+ */
+Repoblar.prototype.getChild = function(el){
+ return document.querySelector(el.getAttribute('data-child'));
+};
+
+/**
+ * @name Repoblar#bindCascade
+ * @method
+ * @param {Object} el element type SELECT 
+ * @param {Object|null} parent Parent node, null if root
+ * @returns this
+ */
+Repoblar.prototype.bindAll = function(){
+  let el = this.root;
+  let child = document.querySelector(this.getChild(el));
+  let $this = this;
+ 
+  do{
+     el.addEventListener('change', (event) => {
+     if(el.options[el.selectedIndex].value==''){
+      child.length=1;
+     }else{
+      $this.populateChild(child, el.options[el.selectedIndex].value)
+     }
+     el= child;
+     child= document.querySelector($this.getChild(el));
+    });
+   } while (child !==null );
+};
+
+/**
  * @name Repoblar#getSelector
  * @method
  * @param {Object} el element type SELECT 
  * @param {Object|null} parent Parent node, null if root
  * @returns this
  */
-Repoblar.prototype.getSelector = function(el, parent){
+Repoblar.prototype.getData = function(el, parent){
      return {
        self: el,
        parent: parent,
+       type: document.getElementobject.getAttribute('data-type'),//json/hxr/id
        source: document.getElementobject.getAttribute('data-source'),
        parameters: el.getAttribute('data-parameters'),
        child:el.getAttribute('data-child')
       };
 };
-
 
 let repoblar = new Repoblar('countrySelector');
