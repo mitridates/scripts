8375481821f2e302b8d1c7e8ec606e63301c62eb
mitridates <mitridates@gmail.com>
2019-12-31 20:57:46 +0100

    Update myform.js

--- myform.js
+++ myform.js
@@ -28,23 +28,45 @@
     /**
      * Enable/disable childNodes
      * @param {bool|null} dsbl
-     * @returns {Object} this
+     * @returns self
      */
     disable: function(dsbl){
-        let obs = methods.toArray.apply(this), length =  obs.length;
-        for(var i = 0; i< obs.length; i++){
+        let obs = methods.toArray.apply(this), lngth =  obs.length;
+        for(var i = 0; i< lngth; i++){
             obs[i].disabled = typeof dsbl === "boolean" ? dsbl :  !obs[i].disabled;
         }
-
-/*        $(':input', this._selector) //this._selector {Object} Form input? selector
-            .not(':button, :submit, :reset, :hidden').each(
-            function () {
-                this.disabled = typeof dsbl === "boolean" ? dsbl :  !this.disabled;
-            }
-        );*/
-
         return this;
-    },        
+    },
+   /**
+    * Clear form/childs node values
+    * @example :
+    *           ('#form, #formid2, ...').myform('clear')
+    *           Grot('#tableid').form('clear')
+    * @returns self
+    */
+       clear: function(){
+        let _clear = function (e) {
+           e.classList.contains('select2')?  $(e).val(null).trigger('change') : e.value = '';
+           $(e).removeAttr('checked').removeAttr('selected');
+        }
+
+        if($(this).is('input, select, checkbox')){//es 1-n objetos : [selector, ...]
+
+          $(this._selector).each(function () {
+              _clear(this);
+          });
+
+        }else{ //todos los chids input dentro de un elemento del dom
+
+            $(':input', this._selector)
+                .not(':button, :submit, :reset, :hidden')
+                .each(function () {
+                _clear(this);
+            });
+        }
+        //        $(this._selector).find('input:radio, input:checkbox, input[type=text], input[type=password], input[type=file], select, textarea').val('');//select selectivo
+        return this;
+    },
        
     };
 
