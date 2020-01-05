43d900d0883bf1cef1a13cce729702daad55424b
mitridates <mitridates@gmail.com>
2020-01-05 18:35:53 +0100

    Update myform.js

--- myform.js
+++ myform.js
@@ -1,42 +1,42 @@
 (function( $ ){
 
-    var methods = {
-        init : function(options) {
-
-        },
-        /**
-         * Retorna el objeto/objetos input de un selector como array
-         * @returns {Array}
-         */
-        toArray : function( ) {  
-            let notin = [':button', ':submit', ':reset', ':hidden'], arr = [];
-            $(':input', this).not(notin.join(', ')).each( function(){ arr.push(this); });
-            return arr;
-        },
-    /**
-     * Enable/disable childNodes
-     * @param {bool|null} dsbl
-     * @returns self
-     */
-    disable: function(dsbl){
-    	[].map.call(methods.toArray.apply(this), function(obj) {
-			obj.disabled = typeof dsbl === "boolean" ? dsbl :  !obj.disabled;
-    	});
-        return this;
-    },
-   /**
-    * Clear form/childs node values
-    * @example :
-    *           ('#form, #formid2, ...').myform('clear')
-    *           Grot('#tableid').form('clear')
-    * @returns self
-    */
-       clear: function(){
-    	[].map.call(methods.toArray.apply(this), function(obj) {
-               obj.classList.contains('select2')?  $(obj).val(null).trigger('change') : obj.value = '';
-               $(obj).removeAttr('checked').removeAttr('selected');
-       });
-        return this;
+var methods =
+	init : function(options) {
+		//??
+	},
+	/**
+	 * Retorna el objeto/objetos input de un selector como array
+	 * @returns {Array}
+	 */
+	toArray : function( ) {  
+	    let notin = [':button', ':submit', ':reset', ':hidden'], arr = [];
+	    $(':input', this).not(notin.join(', ')).each( function(){ arr.push(this); });
+	    return arr;
+	},
+	/**
+	* Enable/disable childNodes
+	* @param {bool|null} dsbl
+	* @returns self
+	*/
+	disable: function(dsbl){
+		[].map.call(methods.toArray.apply(this), function(obj) {
+				obj.disabled = typeof dsbl === "boolean" ? dsbl :  !obj.disabled;
+		});
+		return this;
+	},
+	/**
+	 * Clear form/childs node values
+	 * @example :
+	 *           ('#form, #formid2, ...').myform('clear')
+	 *           Grot('#tableid').form('clear')
+	 * @returns self
+	 */
+	clear: function(){
+		[].map.call(methods.toArray.apply(this), function(obj) {
+		obj.classList.contains('select2')?  $(obj).val(null).trigger('change') : obj.value = '';
+		$(obj).removeAttr('checked').removeAttr('selected');
+		});
+		return this;
     },
        
     };
