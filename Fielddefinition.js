b9db6785a6f41e42001c877cb04cc5fbf117a486
mitridates <mitridates@gmail.com>
2022-02-13 18:08:50 +0100

    Update Fielddefinition.js

--- Fielddefinition.js
+++ Fielddefinition.js
@@ -2,231 +2,103 @@
  * Immediately-Invoked Function Expression (IIFE).
  * @function
  * @param {object} window - Global window object.
- * @returns {Object} window.{Fielddefinityion, Fieldvaluecode}
+ * @returns {Object} window.{Fielddefinition, Fieldvaluecode}
  */
-(function(window, JXhrpost ,JsonApiManager, JsonApiSpec, undefined) {
+(function(window, JsonApiSpec, undefined) {
     'use strict';
+    
     const CODING={
       I: 'International coding',
       U: 'Uncoded',
       L = 'Locally coded' 
     }
-     
-     S = Single-valued   M = Multi-valued
+    const VALUETYPE={
+      S: 'Single-valued',
+      M: 'Multi-valued'
+    };
+    const DATATYPE={
+                'A' : 'Alphanumeric up to 255 chars long (A1-A255)',
+                'N' : 'Numeric; A "real" number allowing decimal places and many significant digits, typically around 15.',
+                'D' : 'Date. Accepts only full valid dates',
+                'L' : 'Logical. True or False',
+                'I' : 'Integer (short); A whole number in the range -32,767 to +32,768.',
+                'S' : 'Short integer up to 32768',
+                'M' : 'Memo ;variable length free text',
+                'B' : 'BLOB; A Binary Large Object e.g. a photo image'
+    }
+    const ENTITY={
+                AR:  'Article in a publication',
+                AT:  'Field or attribute',
+                AV:  'Field value',
+                CA:  'Cave or karst feature',
+                EN:  'Entity',
+                OR:  'Organisation',
+                PA:  'Land parcel',
+                PB:  'Publication',
+                PE:  'Person',
+                PH:  'Photograph',
+                PL:  'Plan or map',
+                PM:  'Marker (Permanent mark)',
+                PS:  'Map series',
+                RE:  'Region or area',
+                RL:  'Role',
+                RP:  'Report',
+                SM:  'Specimen',
+                SP:  'Species',
+                ST:  'Site',
+                SU:  'Subject',
+                SV:  'Survey',
+                SY:  'System field',
+                XK:  'A key-in batch',
+                XL:  'An upload batch',
+                XU:  'An update batch'
+    }
+
+        //##### Fieldvaluecode    ####
     /**
-     * Value & codes
-     * @constructor
+     * Fieldvaluecode class.
      * @name Fieldvaluecode
-     * @param {Object} - JSON:API resource object
-     * @see https://kid.caves.org.au/kid/doc/codes
-     */
-    function Fieldvaluecode(data){
-        this.id = data.id;
-        this.coding= data.coding;
-        this.codes = {};
-        this.links = data.links||null;
-        this.relationships= data.relationships||null;
-        this.toString= function (){
-            return this.id;
-        }
-    }
-
-    /**
-     * @memberof JsonApiSpec
-     * @return {string|null} url
-     */
-    JsonApiSpec.prototype.toLink= function (){
-        return (this.links && this.links.hasOwnProperty('self')) ? this.links.self : null;
-    }
-    
-    /**
-     * @memberof JsonApiSpec
-     * @return {string|null} href
-     */    
-    JsonApiSpec.prototype.toAnchor= function (){
-        let a= this.toLink(),
-            name= this.toString === Object.prototype.toString ? this.id : this.toString();//No hay link o el método toString no ha sido sobreescrito
-        return a? ['<a href="',a,'">', name,'</a>'].join('') : null;
-    }
-    
-    /**
-     * Get attribute|[attributes]|property by name or function. Function can returns compound values
-     * @memberof JsonApiSpec
-     * @param {string|*} s
-     * @return {string|*}
-     */    
-    JsonApiSpec.prototype.get= function (s){
-        let i, ret=[];
-        switch (typeof s){
-            case "string":
-                if(s==='id'){
-                    return this.id
-                }else if(s==='type'){
-                    return this.type
-                }else{
-                    return this.attributes[s];
-                }
-            case "function": return  s.call(this, this.attributes);
-        }
-
-        if(Array.isArray(s)){
-            for(i=0; i<s.length; i++){
-                ret[i]=this.get(s[i])
-            }
-            return ret
-        }
-        return null;
-    }
-
-    /**
-     * Set attribute
-     * @memberof JsonApiSpec
-     * @param {string} key
-     * @param {string|*} val     
-     * @return {JsonApiSpec}
-     */       
-    JsonApiSpec.prototype.set= function (key, val){
-        this.attributes[key]= val;
-        return this;
-    }
-    //##### End JsonApiSpec     ####
-
-    //#####    jsonApiManager    ####
-
-    /**
-     * Intermediary between XMLHttpRequest response and JsonApiSpec
-     * @constructor
-     * @name JsonApiManager
-     * @param {Array} data - response.data
-     * @param {Array|null} included - response.included
-     */
-    function JsonApiManager(data, included) {
-        this.data = data;
-        this.included = included||null;
-        this.ret= [];
-        this.entities= []
-    }
-
-    /**
-     * Set instance of JsonApiSpec
-     * @memberof JsonApiManager
-     * @param {Object} ob - Single JSON:API resource object
-     * @return {JsonApiSpec}
-     */
-    JsonApiManager.prototype.toJsonApiSpec= function(ob){
-        let className= ob.type.charAt(0).toUpperCase() + ob.type.slice(1);
-        switch (className){
-            case 'Person': return new Person(ob);
-            default: return new JsonApiSpec(ob)
-        }
-    }
-
-    /**
-     * Create instance of JsonApiSpec if included
-     * @memberof JsonApiManager
-     * @param {Object} rel - Relationship{id, type}
-     * @return {JsonApiSpec}
-     */
-    JsonApiManager.prototype.getIncluded= function(rel){
-        let inc;
-        for (let key in this.included){
-            inc= this.included[key]
-            if (inc.type===rel.type && inc.id===rel.id) {
-                return this.toJsonApiSpec(inc)
-            }
-        }
-        //posible null???
-        return null
-        //return new JsonApiSpec(inc)
-    }
-
-    /**
-     * Parse XMLHttpRequest Response
-     * @memberof JsonApiManager
-     * @return {Array}
-     */
-    JsonApiManager.prototype.parseResponse = function() {
-        let i,spec, inc;
-
-        for(i=0; i<this.data.length;i++){//loop response.data
-            spec= this.toJsonApiSpec(this.data[i])
-
-            for (let key in spec.attributes){
-                if(spec.relationships && spec.relationships[key]){
-                    //podría devolver null this.getIncluded() ?
-                    // inc = this.getIncluded(spec.relationships[key].data)
-                    // if(inc){
-                    //     spec.attributes[key]= inc
-                    //
-                    // }
-                    spec.attributes[key] = this.getIncluded(spec.relationships[key].data)
-                }
-            }
-            this.ret.push(spec)
-        }
-        return this.ret;
-    }
-
-    /**
-     * Retorna un arreglo de valores de acuerdo a las claves solicitadas
-     * @memberof JsonApiManager
-     * @param {Array} fields - Claves del objeto a devolver
-     * @param {Array} fn - array de pares {key:function(JsonApiSpec, value)} que permite retornar un valor calculado para dicha clave.
-     *                      key debe encontrarse en el array fields
-     * @return {Array}
-     */
-    JsonApiManager.prototype.get = function(fields= [], fnArr=null) {
-        let i, ii, el, field, val,
-            ret=[],
-            arr=[],
-            parsed= this.ret.length? this.ret : this.parseResponse();
-
-        if(arguments.length===0) return this.parsed
-        
-        for(i=0; i<parsed.length; i++){
-            el= parsed[i] //JsonApiSpec
-            arr=[]
-
-            for(ii=0; ii<fields.length; ii++){
-
-                field= fields[ii]
-                val= el.get(field)//puede ser string|object
-
-                if(fnArr && fnArr.hasOwnProperty(field)){//puede ser un valor compuesto
-                    arr.push(fnArr[field].call(el, val))
-                }else if(val instanceof JsonApiSpec){//utilizamos toString
-                    arr.push(val.toString())
-                }else{
-                    arr.push(val)
-                }
-            }
-            ret.push(arr)
-         }
-        return ret;
-    }
-
-    //##### End jsonApiManager    ####
-    
-    //##### Person    ####
-    /**
-     * Person class.
-     * @name Person
      * @constructor
      * @augments JsonApiSpec
      * @throws ExceptionInvalidType
      */
-    function Person(data) {
+    function Fieldvaluecode(data) {
         JsonApiSpec.call(this, data);
-        isExpectedEntityType(this.type, 'person')
-        this.toString= function (){
-            return this.attributes.name+' '+this.attributes.surname;
+        isExpectedEntityType(this.type, 'fieldvaluecode')
+        this.getCoding= function(){
+            let type= this.get('coding')
+            return type && CODING.hasOwnProperty(type)? CODING[type] : type;            
         }
-
+       
     }
     
-    Person.prototype = Object.create(JsonApiSpec.prototype);
+    Fieldvaluecode.prototype = Object.create(JsonApiSpec.prototype);
+    
+    
+        //##### Fielddefinition    ####
+    /**
+     * Fielddefinition class.
+     * @name Fielddefinition
+     * @constructor
+     * @augments JsonApiSpec
+     * @throws ExceptionInvalidType
+     */
+    function Fielddefinition(data) {
+        JsonApiSpec.call(this, data);
+        isExpectedEntityType(this.type, 'fielddefinition')
+        
+        this.getValueType= function(){
+            let type= this.get('valuetype')
+            return type && VALUETYPE.hasOwnProperty(type)? VALUETYPE[type] : type;
+        }
+        this.getDataType= function(){
+            let type= this.get('datatype')
+            return type && DATATYPE.hasOwnProperty(type)? DATATYPE[type] : type;
+        }
+    }
+    Fielddefinition.prototype = Object.create(Fieldvaluecode.prototype);
 
-    window.JsonApiManager= JsonApiManager;
-    window.JsonApiSpec= JsonApiSpec;
+    window.Fieldvaluecode= Fieldvaluecode;
+    window.Fielddefinition= Fielddefinition;
 
-}(window, JXhrpost ,JsonApiManager, JsonApiSpec));
+}(window, JsonApiSpec));
